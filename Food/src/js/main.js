aad3a54799fcada743fc0b690df8407d048ea73b
Oleg <savochkinoleg17@gmail.com>
2023-03-20 19:00:40 +0300

    build: refac code with class

--- Food/src/js/main.js
+++ Food/src/js/main.js
@@ -158,21 +158,79 @@ window.addEventListener('DOMContentLoaded', () => {
         modal.style.cssText = 'display: block;';
         document.body.style.overflow = 'hidden';
         closeElement(modalCloseBtn, modal);
-        clearInterval(modelTimerId);
+        // clearInterval(modelTimerId);
     }
 
 
     showElementByBtns(modalTrigger, modal, modalCloseBtn, _openModal);
 
-    const modelTimerId = setTimeout(_openModal, 3000);
+    // const modelTimerId = setTimeout(_openModal, 3000);
 
     function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
             _openModal();
             window.removeEventListener('scroll', showModalByScroll);
-            clearInterval(modelTimerId)
+            // clearInterval(modelTimerId)
         }
     }
 
     window.addEventListener('scroll', showModalByScroll);
+
+    // Class for Cards
+
+    class MenuItem{
+        constructor(src, alt, subtitle, describe, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.subtitle = subtitle;
+            this.describe = describe;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.currency = 36;
+            this.changeToUAH();
+        }
+
+        changeToUAH(){
+            this.price *= this.currency
+        }
+
+        render(){
+            const element = document.createElement('div');
+
+            element.innerHTML = `<div class="menu__item">
+                <img src="${this.src}" alt="${this.alt}">
+                <h3 class="menu__item-subtitle">${this.subtitle}</h3>
+                <div class="menu__item-descr">${this.describe}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            </div>`;
+            this.parent.append(element);
+        }
+    }
+
+    new MenuItem('img/tabs/vegy.jpg',
+        'vegy',
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих\n' +
+        'овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой\n' +
+        'и высоким качеством!',
+        30,
+        '.menu .container').render();
+
+    new MenuItem('img/tabs/elite.jpg',
+        'elite',
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода ресторан!',
+        20,
+        '.menu .container').render();
+
+    new MenuItem('img/tabs/post.jpg',
+        'post',
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        10,
+        '.menu .container').render();
 });
